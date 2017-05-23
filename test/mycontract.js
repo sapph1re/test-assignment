fd811c2a6425685a65aaebed2a0aa23be41a1e45
Patryk Dwórznik <patryk@dworznik.com>
2017-05-23 18:40:34 +0200

    Add Truffle project

---
+++ test/mycontract.js
@@ -0,0 +1,11 @@
+// var MyContract = artifacts.require("./MyContract.sol");
+
+contract('MyContract', function(accounts) {
+
+  it("should deploy", function() {
+    // return MyContract.deployed().then(function(instance) {
+    //   assert.notEqual(instance, null);
+    // })
+  });
+
+})
